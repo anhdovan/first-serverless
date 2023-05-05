fa8109b4d402bda960d08c3f5e3d35f9d22303f5
anhdovan <dvanh1974@yahoo.com>
2023-05-05 19:25:20 +0700

    test netlify cd/ci

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,7 +2,8 @@ exports.handler = async () => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello world!'
+            message: 'Hello from AnhDV',
+            data: 'THIS IS SOME DATA TO TEST CD/CI'
         })
     }
 }
