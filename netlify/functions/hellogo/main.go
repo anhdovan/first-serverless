391f390e6264de402fb05fb684977267d4f24070
anhdovan <dvanh1974@yahoo.com>
2023-05-05 22:24:15 +0700

    test netlify cd/ci 5

---
+++ netlify/functions/hellogo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+	msg := "msg from client app - " + cc.Client.AppTitle
+	data := fmt.Sprintf("{\"msg\": \"%s\"}", msg)
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       data,
+		//Body:       "Hello, " + cc.Client.AppTitle,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
